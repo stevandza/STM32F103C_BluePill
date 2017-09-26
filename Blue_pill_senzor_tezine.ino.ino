38ee4b42639472ab8aa2b2d380c149f0018a7d46
stevandza <stevandza@gmail.com>
2017-09-26 21:24:44 +0200

    Add files via upload

---
+++ Blue_pill_senzor_tezine.ino.ino
@@ -0,0 +1,27 @@
+#include <Q2HX711.h>
+#include <Wire.h>
+
+
+const byte hx711_data_pin = PA11;
+const byte hx711_clock_pin =PA12;
+
+ #define izlazPWM    PA1
+
+  Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
+
+void setup() {
+ 
+  pinMode(izlazPWM, PWM);
+  Serial.begin(19200);
+
+}
+
+void loop() {
+  
+  
+ Serial.println((hx711.read()/100.0-83200)*24-2700);
+
+  pwmWrite(izlazPWM, (hx711.read()/100.0-83200)*24-2700);
+
+  delay(2);
+}
